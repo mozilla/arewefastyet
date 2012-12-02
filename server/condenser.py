78f2a3ffdc446fc0ac0de0cefbb61fbc8383ef8d
AreWeFastYet <awfy@awfy.(none)>
2012-12-02 04:18:57 +0000

    Condenser now has full aggregation support.

--- server/condenser.py
+++ server/condenser.py
@@ -7,6 +7,7 @@ import json
 from profiler import Profiler
 
 SecondsPerDay = 60 * 60 * 24
+MaxRecentRuns = 30
 
 def export(name, j):
     path = os.path.join(awfy.path, name)
@@ -54,7 +55,8 @@ def split_into_days(timelist):
 
     return days
 
-# Aggregate the datapoints in a graph into the supplied regions.
+# Aggregate the datapoints in a graph into the supplied regions. Line ordering
+# stays the same.
 def condense_graph(graph, regions):
     # Prefill the new graph.
     new_graph = { 'direction': graph['direction'],
@@ -87,7 +89,7 @@ def condense_graph(graph, regions):
         new_graph['lines'].append(newline)
 
     for start, end in regions:
-        new_graph['timelist'].append(start)
+        new_graph['timelist'].append(graph['timelist'][start])
 
     return new_graph
 
@@ -100,6 +102,95 @@ def condense_month(cx, suite, graph, prefix, name):
         }
     export(name + '.json', j)
 
+def combine(graphs):
+    combined = { 'lines': [],
+                 'timelist': [],
+                 'direction': graphs[0]['direction']
+               }
+
+    # Pre-fill modes.
+    modes = { }
+    for graph in graphs:
+        for line in graph['lines']:
+            if line['modeid'] in modes:
+                continue
+            obj = { 'modeid': line['modeid'],
+                    'data': []
+                  }
+            modes[line['modeid']] = obj
+            combined['lines'].append(obj)
+
+    for graph in graphs:
+        updated = { }
+        for line in graph['lines']:
+            newline = modes[line['modeid']]
+            newline['data'].extend(line['data'])
+            updated[line['modeid']] = True
+
+        for mode in modes:
+            if mode in updated:
+                continue
+            empty = [None] * len(graph['timelist'])
+            modes[mode]['data'].extend(empty)
+
+        combined['timelist'].extend(graph['timelist'])
+
+    # Sanity check.
+    for line in combined['lines']:
+        if len(line['data']) != len(combined['timelist']):
+            raise Exception('corrupt graph')
+
+    return combined
+
+def aggregate(graph):
+    graph['aggregate'] = True
+
+    # If we don't have enough points for a historical view, we won't display
+    # a historical view.
+    if len(graph['timelist']) <= MaxRecentRuns:
+        if len(graph['timelist']) == 0:
+            graph['earliest'] = 0
+        else:
+            graph['earliest'] = graph['timelist'][0]
+        return graph
+
+    # If the number of historical points is <= the number of recent points,
+    # then the graph is about split so we don't have to do anything.
+    historical = len(graph['timelist']) - MaxRecentRuns
+    if historical <= MaxRecentRuns:
+        graph['earliest'] = graph['timelist'][historical]
+        return graph
+
+    # How big should each region be?
+    region_length = float(historical) / MaxRecentRuns
+
+    pos = 0
+    regions = []
+    for i in range(0, MaxRecentRuns):
+        start = int(round(pos))
+
+        j = start
+        while j < pos + region_length and j < historical:
+            j = j + 1
+        regions.append((start, j))
+        pos += region_length
+
+    new_graph = condense_graph(graph, regions)
+    for i, line in enumerate(new_graph['lines']):
+        oldline = graph['lines'][i]
+        line['data'].extend(oldline['data'][historical:])
+    new_graph['timelist'].extend(graph['timelist'][historical:])
+
+    new_graph['earliest'] = graph['timelist'][historical]
+    new_graph['aggregate'] = True
+
+    # Sanity check.
+    for line in new_graph['lines']:
+        if len(line['data']) != len(new_graph['timelist']):
+            raise Exception('corrupt graph')
+
+    return new_graph
+
 def condense(cx, suite, prefix, name):
     sys.stdout.write('Importing all datapoints for ' + name + '... ')
     sys.stdout.flush()
@@ -107,6 +198,9 @@ def condense(cx, suite, prefix, name):
         graphs = find_all_months(cx, prefix, name)
         diff = p.time()
     print('took ' + diff)
+
+    if not len(graphs):
+        return
 
     for when, graph in graphs:
         new_name = prefix + 'condensed-' + name + '-' + str(when[0]) + '-' + str(when[1])
@@ -117,20 +211,47 @@ def condense(cx, suite, prefix, name):
             diff = p.time()
         print(' took ' + diff)
 
+    # Combine everything.
+    sys.stdout.write('Aggregating ' + name + '... ')
+    sys.stdout.flush()
+    with Profiler() as p:
+        combined = combine([graph for when, graph in graphs])
+        summary = aggregate(combined)
+        diff = p.time()
+    print('took ' + diff)
+
+    return summary
 
 def condense_suite(cx, machine, suite):
     name = suite.name + '-' + str(machine.id)
-    condense(cx, suite, '', name)
+
+    # We don't build individual aggregate json for each suite, so just pass
+    # the combine graph back to our caller.
+    suite_aggregate = condense(cx, suite, '', name)
 
     for test in suite.tests:
-        name = suite.name + '-' + test + '-' + str(machine.id)
-        condense(cx, suite, 'bk-', name)
+        test_name = suite.name + '-' + test + '-' + str(machine.id)
+        test_aggregate = condense(cx, suite, 'bk-', test_name)
+        j = { 'version': awfy.version,
+              'graph': test_aggregate
+            }
+        export('bk-aggregate-' + test_name + '.json', j)
 
+    return suite_aggregate
 
 def condense_all(cx):
     for machine in cx.machines:
+        aggregates = { }
         for suite in cx.benchmarks:
             if suite.name == 'v8':
                 continue
-            condense_suite(cx, machine, suite)
+            suite_aggregate = condense_suite(cx, machine, suite)
+            if suite.name == 'misc':
+                continue
+            aggregates[suite.name] = suite_aggregate
+
+        j = { 'version': awfy.version,
+              'graphs': aggregates
+            }
+        export('aggregate-' + str(machine.id) + '.json', j)
 
