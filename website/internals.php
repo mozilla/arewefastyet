9e0debaf4dc900c53bf889418b629a0470316c53
David Anderson <danderson@mozilla.com>
2013-11-19 19:45:08 +0000

    Fix update bug on the website.

--- website/internals.php
+++ website/internals.php
@@ -62,7 +62,7 @@ function find_or_add_test($suite_id, $name)
     $query = "select id from awfy_suite_test where suite_id = $suite_id and name = '$name'";
     $results = mysql_query($query);
     if (!$results || mysql_num_rows($results) < 1) {
-        $query = "insert into awfy_suite_test (suite_id, name) values($suite_id, $name)";
+        $query = "insert into awfy_suite_test (suite_id, name) values($suite_id, '$name')";
         mysql_query($query);
         return mysql_insert_id();
     }
