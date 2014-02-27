8537eb41695dbd448f07ded236d3a138e9074a2e
Niko Matsakis <niko@alum.mit.edu>
2014-02-27 12:10:38 -0500

    Add an incredibly simple micro benchmark for typed objects, and a corresponding one based on standard JS arrays

---
+++ benchmarks/misc/tests/assorted/misc-typedobj-utf8array-standard.js
@@ -0,0 +1,19 @@
+// Paired with misc-typedobj-utf8array-standard.js
+
+function do_test() {
+  // Hand tweaked to get something around 1 seconds on my laptop.
+  var len = 192 * 100 * 1024;
+  var jsArray_in = new Array();
+  var jsArray_out = new Array();
+  for(var i = 0; i < len; i++) {
+    jsArray_in[i] = 1;
+  }
+  var sum = 0;
+  for(k = 0; k < 15; k++) {
+    for(i = 0; i < len; i++) {
+      jsArray_out[i] = jsArray_in[i];
+    }
+  }
+}
+do_test();
+
---
+++ benchmarks/misc/tests/assorted/misc-typedobj-utf8array-typedobj.js
@@ -0,0 +1,20 @@
+// Paired with misc-typedobj-utf8array-typedobj.js
+
+function do_test() {
+  // Hand tweaked to get something around 1 seconds on my laptop.
+  var len = 192 * 100 * 1024;
+  var BDType = TypedObject.uint8.array(len);
+  var bdArray_in = new BDType();
+  var bdArray_out = new BDType();
+  for(var i = 0; i < len; i++) {
+    bdArray_in[i] = 1;
+  }
+  var sum = 0;
+  for(var k = 0; k < 15; k++) {
+    for(i = 0; i < len; i++) {
+      bdArray_out[i] = bdArray_in[i];
+    }
+  }
+}
+do_test();
+
