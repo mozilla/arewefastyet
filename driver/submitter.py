e154418134d8046a27fac3a4232ebd4b045b5cca
Hannes Verschore <hv1989@gmail.com>
2014-03-19 10:51:20 +0100

    Enable submitting to multiple urls

--- driver/submitter.py
+++ driver/submitter.py
@@ -10,52 +10,61 @@ import urllib2
 
 class Submitter:
     def __init__(self):
-        self.url = utils.config.get('main', 'updateURL')
         self.machine = utils.config.get('main', 'machine')
+        self.urls = utils.config.get('main', 'updateURL').split(",")
+        self.runIds = []
+        for i in range(len(self.urls)):
+            self.urls[i] = self.urls[i].strip()
+            self.runIds.append(0)
 
     def Awake(self):
-        url = self.url
-        url += '?awake=yes'
-        url += '&MACHINE=' + str(self.machine)
-        urllib2.urlopen(url)
+        for i in range(len(self.urls)):
+            url = self.urls[i]
+            url += '?awake=yes'
+            url += '&MACHINE=' + str(self.machine)
+            urllib2.urlopen(url)
 
     def Start(self):
-        url = self.url
-        url += '?run=yes'
-        url += '&MACHINE=' + str(self.machine)
-        url = urllib2.urlopen(url)
-        contents = url.read()
-        m = re.search('id=(\d+)', contents)
-        if m == None:
-            raise Exception('Remote error: ' + contents)
-        self.runID = int(m.group(1))
+        for i in range(len(self.urls)):
+            url = self.urls[i]
+            url += '?run=yes'
+            url += '&MACHINE=' + str(self.machine)
+            url = urllib2.urlopen(url)
+            contents = url.read()
+            m = re.search('id=(\d+)', contents)
+            if m == None:
+                raise Exception('Remote error: ' + contents)
+            self.runIds[i] = int(m.group(1))
 
     def AddEngine(self, name, cset):
-        args = { 'run': 'addEngine',
-                 'runid': str(self.runID),
-                 'name': name,
-                 'cset': cset
-               }
-        url = self.url + '?' + urllib.urlencode(args)
-        urllib2.urlopen(url)
+        for i in range(len(self.urls)):
+            args = { 'run': 'addEngine',
+                     'runid': str(self.runIds[i]),
+                     'name': name,
+                     'cset': cset
+                   }
+            url = self.urls[i] + '?' + urllib.urlencode(args)
+            urllib2.urlopen(url)
 
     def AddTests(self, tests, suite, mode):
         for test in tests:
             self.SubmitTest(test['name'], suite, mode, test['time'])
 
     def SubmitTest(self, name, suite, mode, time):
-        url = self.url
-        url += '?name=' + name
-        url += '&run=' + str(self.runID)
-        url += '&suite=' + suite
-        url += '&mode=' + mode
-        url += '&time=' + str(time)
-        urllib2.urlopen(url)
+        for i in range(len(self.urls)):
+            url = self.urls[i]
+            url += '?name=' + name
+            url += '&run=' + str(self.runIds[i])
+            url += '&suite=' + suite
+            url += '&mode=' + mode
+            url += '&time=' + str(time)
+            urllib2.urlopen(url)
 
     def Finish(self, status):
-        url = self.url
-        url += '?run=finish'
-        url += '&status=' + str(status)
-        url += '&runid=' + str(self.runID)
-        urllib2.urlopen(url)
+        for i in range(len(self.urls)):
+            url = self.urls[i]
+            url += '?run=finish'
+            url += '&status=' + str(status)
+            url += '&runid=' + str(self.runIds[i])
+            urllib2.urlopen(url)
 
