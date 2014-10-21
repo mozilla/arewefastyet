d5153372ca1bd0c56cd28722214e13d72ad64dd3
Hannes Verschore <hv1989@gmail.com>
2014-10-21 17:27:55 +0200

    Update benchmark.py

--- android-driver/benchmark.py
+++ android-driver/benchmark.py
@@ -72,6 +72,15 @@ class Dromaeo(Benchmark):
     def __init__(self):
         Benchmark.__init__(self, "dromaeo", "1.0", "desktop-driver/dromaeo.html")
 
+    def processResults(self, results):
+        ret = []
+        for key in results:
+            if key == "total":
+                ret.append({'name': "__total__", 'time': results[key]})
+            else:
+                ret.append({'name': key, 'time': results[key]})
+        return ret
+
 Benchmarks = [Octane(), SunSpider(), Kraken(), WebGLSamples()]
 
 # Test if server is running and start server if needed.
