292c18500978eb2f2533999f064e36ff9df285df
Brian Hackett <bhackett1024@gmail.com>
2014-11-06 10:24:26 -0700

    Fix typed object test for platforms without TypedObject defined.

--- benchmarks/misc/tests/assorted/misc-typedobj-simple-struct-typedobj.js
+++ benchmarks/misc/tests/assorted/misc-typedobj-simple-struct-typedobj.js
@@ -1,31 +1,34 @@
 // Paired with misc-typedobj-simple-struct-standard.js
 
-var T = TypedObject;
+if (typeof TypedObject != "undefined") {
+    var T = TypedObject;
 
-var Struct = new T.StructType({a: T.int32, b:T.int32});
-var ObjectArray = T.Object.array(100000);
+    var Struct = new T.StructType({a: T.int32, b:T.int32});
+    var ObjectArray = T.Object.array(100000);
 
-function do_test() {
-    var arr1 = new ObjectArray();
-    var arr2 = new ObjectArray();
-    for (var i = 0; i < arr1.length; i++) {
-        arr1[i] = new Struct();
-        arr1[i].a = 1;
-        arr1[i].b = 2;
-        arr2[i] = new Struct();
-        arr2[i].a = 2;
-        arr2[i].b = 1;
-    }
+    function do_test() {
+        var arr1 = new ObjectArray();
+        var arr2 = new ObjectArray();
+        for (var i = 0; i < arr1.length; i++) {
+            arr1[i] = new Struct();
+            arr1[i].a = 1;
+            arr1[i].b = 2;
+            arr2[i] = new Struct();
+            arr2[i].a = 2;
+            arr2[i].b = 1;
+        }
 
-    for (var i = 0; i < 1000; i++) {
-        for (var j = 0; j < arr1.length; j++) {
-            var a = arr1[i].a;
-            var b = arr1[i].b;
-            arr1[i].a = arr2[i].b;
-            arr1[i].b = arr2[i].a;
-            arr2[i].a = a;
-            arr2[i].b = b;
+        for (var i = 0; i < 1000; i++) {
+            for (var j = 0; j < arr1.length; j++) {
+                var a = arr1[i].a;
+                var b = arr1[i].b;
+                arr1[i].a = arr2[i].b;
+                arr1[i].b = arr2[i].a;
+                arr2[i].a = a;
+                arr2[i].b = b;
+            }
         }
     }
+
+    do_test();
 }
-do_test();
