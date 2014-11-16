bda3d59d123955b227522a5f181aef1634aa7100
Hannes Verschore <hv1989@gmail.com>
2014-11-16 23:25:21 +0100

    Improve server that retrieves/servers benchmarks + init. work desktop mac slave

---
+++ browser-driver/fullbrowserbenchmarks.py
@@ -0,0 +1,118 @@
+import subprocess
+import socket
+import os
+import time
+import json
+import sys
+
+sys.path.insert(1, '../driver')
+import utils
+
+class Benchmark:
+    def __init__(self, suite, version):
+        self.suite = suite
+        self.version = suite+" "+version
+
+    def run(self, engine, submit):
+        # Run tests.
+        runOneBenchmark = False
+        for modeInfo in engine.modes:
+            if os.path.exists("results"):
+                os.unlink("results")
+
+            engine.run(self.suite + ".localhost:8000")
+            timeout = int(utils.config.get('main', 'timeout')) * 60
+            while not os.path.exists("results") and timeout > 0:
+                time.sleep(10)
+                timeout -= 10
+            engine.kill()
+
+            if timeout <= 0:
+                print "Running benchmark timed out"
+                continue
+
+            fp = open("results", "r")
+            results = json.loads(fp.read())
+            fp.close()
+
+            results = self.processResults(results)
+            submit.AddTests(results, self.suite, self.version, modeInfo["name"])
+
+            runOneBenchmark = True
+        return runOneBenchmark
+
+    def processResults(self, results):
+        return results
+
+class Octane(Benchmark):
+    def __init__(self):
+        Benchmark.__init__(self, "octane", "2.0.1")
+
+    def processResults(self, results):
+        ret = []
+        for key in results:
+            if key == "total":
+                ret.append({'name': "__total__", 'time': results[key]})
+            else:
+                ret.append({'name': key, 'time': results[key]})
+        return ret
+
+class SunSpider(Benchmark):
+    def __init__(self):
+        Benchmark.__init__(self, "ss", "1.0.1")
+
+class Kraken(Benchmark):
+    def __init__(self):
+        Benchmark.__init__(self, "kraken", "1.1")
+
+class WebGLSamples(Benchmark):
+    def __init__(self):
+        Benchmark.__init__(self, "webglsamples", "0.1")
+
+class Dromaeo(Benchmark):
+    def __init__(self):
+        Benchmark.__init__(self, "dromaeo", "1.0")
+        
+    def processResults(self, results):
+        ret = []
+        for key in results:
+            if key == "total":
+                ret.append({'name': "__total__", 'time': results[key]})
+            else:
+                ret.append({'name': key, 'time': results[key]})
+        return ret
+
+class Massive(Benchmark):
+    def __init__(self):
+        Benchmark.__init__(self, "massive", "1.0")
+        
+    def processResults(self, results):
+        ret = []
+        for item in results:
+            if item == None:
+                pass
+            elif item["benchmark"] == "score":
+                ret.append({'name': "__total__", 'time': item["result"]})
+            else:
+                ret.append({'name': item["benchmark"], 'time': item["result"]})
+        return ret
+
+class JetStream(Benchmark):
+    def __init__(self):
+        Benchmark.__init__(self, "jetstream", "1.0")
+        
+    def processResults(self, results):
+        ret = []
+        for item in results:
+            if item == "geomean":
+                ret.append({'name': "__total__", 'time': results[item]["statistics"]["mean"]})
+            else:
+                ret.append({'name': item, 'time': results[item]["statistics"]["mean"]})
+        return ret
+
+# Test if server is running and start server if needed.
+s =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+result = s.connect_ex(("localhost", 8000))
+s.close()
+if result > 0:
+    subprocess.Popen(["python", "server.py"])
