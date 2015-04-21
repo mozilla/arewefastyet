e01ea137ca16abba4f38d68ae2ee7d5c1f4cb04e
Hannes Verschore <hv1989@gmail.com>
2015-04-21 11:01:05 +0200

    Enable async browser tests.:w

--- benchmarks/misc-desktop/tests/assorted/misc-test.js
+++ benchmarks/misc-desktop/tests/assorted/misc-test.js
@@ -1,6 +1,14 @@
 function test() {
    return document.getElementById("test");
 }
+
+
+// Optional. When you don't want to remove the time it took to setup the test.
+startMeasuring();
+
 for (var i=0; i<100000; i++) {
     test();
 }
+
+// Required. Notifies the measuring is done.
+stopMeasuring();
