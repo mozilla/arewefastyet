1fbf350b4f1537d0359808811dd5d2085657e670
Hannes Verschore <hv1989@gmail.com>
2015-10-27 14:43:48 +0100

    Fix migration system

--- database/migration-1.php
+++ database/migration-1.php
@@ -2,10 +2,10 @@
 
 // Demote 'stamp' to a number that will be sequential to base off the sorting.
 
-function migrate() {
+$migrate = function() {
     mysql_query("ALTER TABLE `awfy_run` CHANGE `stamp` `sort_order` INT( 10 ) UNSIGNED NOT NULL ;") or die(mysql_error());
-}
+};
 
-function rollback() {
+$rollback = function() {
     mysql_query("ALTER TABLE `awfy_run` CHANGE `sort_order` `stamp` INT( 10 ) UNSIGNED NOT NULL ;") or die(mysql_error());
-}
+};
