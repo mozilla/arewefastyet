6efcbb7a7852ef04c2f46df8b4f5f147d10544cf
Hannes Verschore <hv1989@gmail.com>
2015-10-27 08:43:53 -0700

    Add possibility to log to slack

--- website/internals.php
+++ website/internals.php
@@ -29,6 +29,7 @@ class Config {
         $this->mysql_password = $config_array["mysql"]["pass"];
         $this->mysql_db_name  = $config_array["mysql"]["db_name"];
         $this->data_folder    = $config_array["general"]["data_folder"];
+        $this->slack_webhook  = $config_array["general"]["slack_webhook"];
     }
 }
 
@@ -37,6 +38,33 @@ function init_database()
     global $config;
     mysql_connect($config->mysql_host, $config->mysql_username, $config->mysql_password) or die("ERROR: " . mysql_error());
     mysql_select_db($config->mysql_db_name) or die("ERROR: " . mysql_error());
+}
+
+// (string) $message - message to be passed to Slack
+// (string) $room - room in which to write the message, too
+// (string) $icon - You can set up custom emoji icons to use with each message
+function slack($message, $room = "engineering", $icon = ":longbox:")
+{
+	global $config;
+
+	$room = ($room) ? $room : "engineering";
+	$data = "payload=" . json_encode(array(
+			#"channel"       =>  "#{$room}",
+			"text"          =>  $message,
+			"icon_emoji"    =>  $icon
+		));
+   
+	// You can get your webhook endpoint from your Slack settings
+	$url = $config->slack_webhook;
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$result = curl_exec($ch);
+	curl_close($ch);
+   
+	return $result;
 }
 
 function username()
