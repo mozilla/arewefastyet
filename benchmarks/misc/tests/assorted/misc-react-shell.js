d6b916d50995643bdbd9a498f761cb112d790cb4
Hannes Verschore <hv1989@gmail.com>
2016-07-06 22:51:08 +0100

    Add an assorted tests with react code

---
+++ benchmarks/misc/tests/assorted/misc-react-shell.js
@@ -0,0 +1,3 @@
+var ReactDOM = React.__SECRET_DOM_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
+var ReactDOMServer = React.__SECRET_DOM_SERVER_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
+ReactDOMServer.renderToString(React.createElement(Benchmark));
