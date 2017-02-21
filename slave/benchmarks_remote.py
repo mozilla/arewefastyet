264a86fcb0ad8e5b8cae1911fd0fe704a13699fc
Hannes Verschore <hv1989@gmail.com>
2017-02-21 15:24:42 +0100

    Increase timeout of Dromaeo to 20minutes

--- slave/benchmarks_remote.py
+++ slave/benchmarks_remote.py
@@ -43,7 +43,7 @@ class Octane(Benchmark):
 
 class Dromaeo(Benchmark):
     def __init__(self):
-        Benchmark.__init__(self, "1.0", 17)
+        Benchmark.__init__(self, "1.0", 20)
         self.url = 'http://' + self.suite + ".localhost:8000/?recommended"
 
     def processResults(self, results):
