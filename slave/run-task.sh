7c4fc81aa2f0f8bfda040f9eacc4486d69f21149
Hannes Verschore <hv1989@gmail.com>
2017-04-25 09:47:59 +0200

    Create run-task.sh

---
+++ slave/run-task.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -f /tmp/awfy-deamon ]
+then
+	echo "Already running";
+	exit;
+fi
+
+touch /tmp/awfy-deamon;
+
+control_c()
+{
+	rm /tmp/awfy-deamon;
+	exit;
+}
+ 
+# trap keyboard interrupt (control-c)
+trap control_c SIGINT
+
+cd arewefastyet
+
+for (( ; ; ))
+do
+
+	python task.py --run <<CONTROL_UNIT_ID>>
+
+done
