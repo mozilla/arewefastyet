85e25b8982858f70f2a22803b1f886823b00d06e
Benjamin Bouvier <public@benj.me>
2017-06-07 11:10:39 +0200

    Auto-update AWFY;

--- slave/task.py
+++ slave/task.py
@@ -12,13 +12,19 @@ if __name__ == "__main__":
     parser.add_option("-r", "--run", dest="machine", type="int", help="Get and run the task of a given (control) machine.")
     (options, args) = parser.parse_args()
 
+    # Update AWFY to the latest version first.
+    utils.run_realtime(['git', 'pull'])
+
+    # Query the next tasks.
     url = "http://www.arewefastyet.com/task.php?unit="+str(options.machine)
     data = urllib2.urlopen(url).read()
     data = json.loads(data)
 
+    # Run the tasks.
     task = data["task"].replace("\r\n", "\n").replace("\r", "\n")
     output = utils.run_realtime(task, shell=True)
 
+    # Report the result.
     url = "http://www.arewefastyet.com/task.php?finish="+str(data["id"])
     req = urllib2.Request(url=url, data=urllib.urlencode({"output": output}));
     urllib2.urlopen(req)
