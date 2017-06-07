21a3437e938b3926479fc56d04d3950f729d63e9
Benjamin Bouvier <public@benj.me>
2017-06-07 14:26:11 +0200

    Add ARES6 to the schedule page;

--- website/lib/DB/TaskRecipe.php
+++ website/lib/DB/TaskRecipe.php
@@ -27,101 +27,102 @@ class TaskRecipe extends DB {
         return $this->select("control_unit_id");
     }
 
-	public function inputs() {
-		$inputs = [];
-		$task = $this->task();
-		if (strpos($task, "{revision}") !== false) {
-			$inputs[] = array(
-				"id" => "revision",
-				"type" =>"alphanum",
-				"name" => "Revision",
-			);
-		}
-		if (strpos($task, "{repo}") !== false) {
-			$inputs[] = array(
-				"id" => "repo",
-				"type" => "select",
-				"name" => "Repository",
-				"options" => ["mozilla-try", "mozilla-inbound", "mozilla-aurora", "mozilla-beta"]
-			);
-		}
-		if (strpos($task, "{shell_repo}") !== false) {
-			$inputs[] = array(
-				"id" => "shell_repo",
-				"type" => "select",
-				"name" => "Repository",
-				"options" => ["mozilla-try", "mozilla"]
-			);
-		}
-		if (strpos($task, "{browser_benchmark}") !== false) {
-			$inputs[] = array(
-				"id" => "browser_benchmark",
-				"type" => "select",
-				"name" => "Benchmark",
-				"options" => ["remote.octane", "remote.dromaeo", "remote.massive",
+    public function inputs() {
+        $inputs = [];
+        $task = $this->task();
+        if (strpos($task, "{revision}") !== false) {
+            $inputs[] = array(
+                "id" => "revision",
+                "type" =>"alphanum",
+                "name" => "Revision",
+            );
+        }
+        if (strpos($task, "{repo}") !== false) {
+            $inputs[] = array(
+                "id" => "repo",
+                "type" => "select",
+                "name" => "Repository",
+                "options" => ["mozilla-try", "mozilla-inbound", "mozilla-aurora", "mozilla-beta"]
+            );
+        }
+        if (strpos($task, "{shell_repo}") !== false) {
+            $inputs[] = array(
+                "id" => "shell_repo",
+                "type" => "select",
+                "name" => "Repository",
+                "options" => ["mozilla-try", "mozilla"]
+            );
+        }
+        if (strpos($task, "{browser_benchmark}") !== false) {
+            $inputs[] = array(
+                "id" => "browser_benchmark",
+                "type" => "select",
+                "name" => "Benchmark",
+                "options" => ["remote.octane", "remote.dromaeo", "remote.massive",
                               "remote.jetstream", "remote.speedometer", "remote.speedometer-misc",
                               "remote.kraken", "remote.sunspider", "remote.browsermark",
-							  "remote.wasm"]
-			);
-		}
-		if (strpos($task, "{shell_benchmark}") !== false) {
-			$inputs[] = array(
-				"id" => "shell_benchmark",
-				"type" => "select",
-				"name" => "Benchmark",
-				"options" => ["shell.octane", "shell.sunspider", "shell.kraken", "shell.assorted",
-                              "shell.asmjsapps", "shell.asmjsmicro", "shell.dart", "shell.sixspeed"]
-			);
-		}
-		if (strpos($task, "{autodir}") !== false) {
-			$inputs[] = array(
-				"id" => "autodir",
-				"type" => "auto",
-			);
-		}
-		return $inputs;
-	}
+                              "remote.wasm"]
+            );
+        }
+        if (strpos($task, "{shell_benchmark}") !== false) {
+            $inputs[] = array(
+                "id" => "shell_benchmark",
+                "type" => "select",
+                "name" => "Benchmark",
+                "options" => ["shell.octane", "shell.sunspider", "shell.kraken", "shell.assorted",
+                              "shell.asmjsapps", "shell.asmjsmicro",
+                              "shell.dart", "shell.sixspeed", "shell.ares6"]
+            );
+        }
+        if (strpos($task, "{autodir}") !== false) {
+            $inputs[] = array(
+                "id" => "autodir",
+                "type" => "auto",
+            );
+        }
+        return $inputs;
+    }
 
-	function fill($values) {
-		$task = $this->task();
-		$inputs = $this->inputs();
-		foreach ($inputs as $input) {
-			if ($input["type"] == "auto" && $input["id"] == "autodir") {
-				if (isset($values["shell_repo"]) &&
+    function fill($values) {
+        $task = $this->task();
+        $inputs = $this->inputs();
+        foreach ($inputs as $input) {
+            if ($input["type"] == "auto" && $input["id"] == "autodir") {
+                if (isset($values["shell_repo"]) &&
                     preg_match("/[a-z]*-[a-z]*/i", $values["shell_repo"]))
-				{
-					$task = str_replace("{autodir}", $values["shell_repo"], $task);
-				} else if (isset($values["repo"]) &&
-                           preg_match("/[a-z]*-[a-z]*/i", $values["repo"]))
-				{
-					$task = str_replace("{autodir}", $values["repo"], $task);
-				} else {
-					$task = str_replace("{autodir}", ".", $task);
-				}
-			}
-			if (!isset($values[$input["id"]]))
-				continue;
+                {
+                    $task = str_replace("{autodir}", $values["shell_repo"], $task);
+                } else if (isset($values["repo"]) &&
+                    preg_match("/[a-z]*-[a-z]*/i", $values["repo"]))
+                {
+                    $task = str_replace("{autodir}", $values["repo"], $task);
+                } else {
+                    $task = str_replace("{autodir}", ".", $task);
+                }
+            }
+            if (!isset($values[$input["id"]]))
+                continue;
 
-			if ($input["type"] == "alphanum" && ctype_alnum($values[$input["id"]])) {
-				$task = str_replace("{".$input["id"]."}", $values[$input["id"]], $task);
-				continue;
-			}
+            if ($input["type"] == "alphanum" && ctype_alnum($values[$input["id"]])) {
+                $task = str_replace("{".$input["id"]."}", $values[$input["id"]], $task);
+                continue;
+            }
 
-			if ($input["type"] == "select" && in_array($values[$input["id"]], $input["options"])) {
-				$task = str_replace("{".$input["id"]."}", $values[$input["id"]], $task);
-				continue;
-			}
-		}
+            if ($input["type"] == "select" && in_array($values[$input["id"]], $input["options"])) {
+                $task = str_replace("{".$input["id"]."}", $values[$input["id"]], $task);
+                continue;
+            }
+        }
 
-		return $task;
-	}
+        return $task;
+    }
 
-	public static function all() {
+    public static function all() {
         $qTasks = mysql_query("SELECT id FROM control_task_recipe") or die(mysql_error());
-		$tasks = Array();
-		while ($task = mysql_fetch_object($qTasks)) {
-			$tasks[] = new TaskRecipe($task->id);
-		}
-		return $tasks;
-	}
+        $tasks = Array();
+        while ($task = mysql_fetch_object($qTasks)) {
+            $tasks[] = new TaskRecipe($task->id);
+        }
+        return $tasks;
+    }
 }
