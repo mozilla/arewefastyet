31efd4b7193b3250a58a2f3f1fc40ed2692513b8
Bob Clary <bob@bclary.com>
2018-01-07 07:56:07 -0800

    Bug 1422887 - AWFY - standardize run-task.sh script, r=bbouvier

--- slave/run-task.sh
+++ slave/run-task.sh
@@ -1,25 +1,64 @@
 #!/bin/bash
 
-if [ -f /tmp/awfy-deamon ]
-then
-    echo "Already running";
-    exit;
+# Execute the arewefastyet task for the control unit
+# specified in the environment variable CONTROL_UNIT_ID.
+
+help()
+{
+    cat <<EOF
+error: Missing CONTROL_UNIT_ID environment variable.
+
+Either specify CONTROL_UNIT_ID in the environment or create
+~/AWFY_CONFIG containing the line:
+
+export CONTROL_UNIT_ID=id
+
+where id is the control unit id for this host.
+
+Then re-run this command.
+EOF
+}
+
+if [[ -e ~/AWFY_CONFIG ]]; then
+    source ~/AWFY_CONFIG
 fi
 
-touch /tmp/awfy-deamon;
+if [[ -z "$CONTROL_UNIT_ID" ]]; then
+    help
+    exit 1
+fi
+
+if pgrep -f run-task.sh; then
+    echo "run-task.sh is already running."
+    exit 2
+fi
 
 control_c()
 {
-    rm /tmp/awfy-deamon;
-    exit;
+    local answer
+    read -e -i Y -p "Kill running task?: Y/N" answer
+    if [[ "$answer" == "Y" ]]; then
+        pkill -9 -f task.py
+    fi
+    echo "Exiting...."
+    exit 2
 }
 
 # trap keyboard interrupt (control-c)
 trap control_c SIGINT
 
-cd arewefastyet
+export DISPLAY=:0
 
-for (( ; ; ))
-do
-    python task.py --run <<CONTROL_UNIT_ID>>
+cd ~/arewefastyet/slave
+
+while true; do
+    # Remove temporary files left behind by Firefox.
+    rm -f /tmp/tmpaddon*
+    echo "$(date): task.py --run $CONTROL_UNIT_ID"
+    if ! python task.py --run $CONTROL_UNIT_ID; then
+        echo "$(date): task.py --run $CONTROL_UNIT_ID: $?"
+    fi
+    echo "$(date): sleep 60"
+    echo "========================================"
+    sleep 60
 done
